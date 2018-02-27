806908f78014c38efdf276d18222e4044e8c8866
mshsheng <mshsheng@126.com>
2018-02-27 21:16:52 +0800

    Add Saving Feature

--- Clustering/__init__.py
+++ Clustering/__init__.py
@@ -3,6 +3,7 @@
 from Clustering.config import configs  # The config file.
 import Clustering.audio_class
 import Clustering.import_file
+import Clustering.save
 import librosa
 
 filelist = []
@@ -17,6 +18,14 @@ def start_culster():
     for i in r:
         audio = audio_class.audio()
         audio.filename = filelist[i][0]
-        y, audio.sr, audio.d = import_file.load_audio_file(filelist[i][1])
+        audio.filepath = filelist[i][1]
+        y, audio.sr, audio.d = import_file.load_audio_file(audio.filepath)
         audio.mfccs = librosa.feature.mfcc(y, audio.sr, n_mfcc=n_mfcc, hop_length=frame_shift, n_fft=frame_size)
         __audiolist.append(audio)
+
+    ####################################
+    ## Here inserts clustering module ##
+    ####################################
+
+    audiolist = []
+    save.save_wav_in_group(audiolist)
